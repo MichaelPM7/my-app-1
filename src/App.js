9b0c8511b4ba9993fd675f3bfd39575ff5cd8ce5
MichaelPM <pacchamm@gmail.com>
2021-11-28 23:47:24 -0500

    Botones
    Botones que cambian entre 'true' y 'false'.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
 //! import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import tasks from "./sample/task.json";
+import Tasks from "./components/Tasks";
 
-function HelloWorld() {
-  return (
-    <><div id='Fuck'>Fuck yours xd</div></>
-  )
-}
-function App() {
-  return (
-    <div>hello world<HelloWorld /><HelloWorld /><HelloWorld /></div>
-  )
+/* {
+  <div>
+  class HelloWorld extends React.Component {
+    state = {
+      show: true,
+    };
+    toggleShow = () => {
+      this.setState({ show: !this.state.show });
+    };
+    render() {
+      if (this.state.show) {
+        return (
+          <div id="Fuck">
+            <h1>{this.props.subtitle}</h1>
+            {this.props.myText}
+            <button onClick={this.toggleShow}>Toggle Show</button>
+          </div>
+        );
+      } else {
+        return (
+          <h1>
+            There are not element{" "}
+            <button onClick={this.toggleShow}>Toggle Show</button>
+          </h1>
+        );
+      }
+    }
+  }
+  function App() {
+    return (
+      <div>
+        hello world
+        <HelloWorld myText="Fuck" subtitle="XD" />
+        <HelloWorld myText="Hi bitch" subtitle=":v" />
+        <HelloWorld myText="2001" subtitle=":3" />
+      </div>
+    );
+  }
+</div>
+} */
+
+class App extends Component {
+  state = {
+    tasks: tasks,
+  };
+  render() {
+    return (
+      <div>
+        <Tasks tasks={this.state.tasks} />
+      </div>
+    );
+  }
 }
 
 export default App;
